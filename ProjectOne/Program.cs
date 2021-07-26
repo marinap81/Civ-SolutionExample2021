194e166c367ac6d4f39583fb74cab673d12e4f60
Anh Nguyen <anguyen@swin.edu.au>
2021-07-27 09:32:25 +1000

    Initial commit

---
+++ ProjectOne/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ProjectPersonClasses;
+
+namespace ProjectOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.Name = "Jon";
+            p1.Id = 12345;
+
+            System.Console.WriteLine(p1.Name);
+        }
+    }
+}
---
+++ ProjectPersonClasses/Person.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ProjectPersonClasses
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+}
